58d995e0754c307fe5ac3469a14e8d100440137f
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-09-30 09:07:23 -0600

    Added a little bit of styles

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,16 @@ const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 const pug = require('gulp-pug');
 const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
     return gulp.src('app/sass/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer({
 			browsers: ['last 2 versions']
-		}))
+        }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream())
 });
