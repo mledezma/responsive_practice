bac881ec7367c8a53602b81c1cf56149479d081d
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-10-12 22:23:00 -0600

    Added the final details

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,11 @@
-var app = (function(){
-    console.log('hi');
+var app = (function() {
     var btnMenu = document.querySelector('#btnMenu');
     var menu = document.querySelector('#menu');
     var opacity = document.querySelector('.opacity');
     var logo = document.querySelector('.logo');
+    var menuContainer = document.querySelector('#menuContainer');
 
-    function init(){
+    function init() {
         btnMenu.addEventListener('click', function() {
             if(menu.classList.contains('hidden')) {
                 btnMenu.classList.add('hidden'); 
@@ -18,14 +18,38 @@ var app = (function(){
                 logo.classList.remove('logo-expanded');
             }
             menu.classList.toggle('hidden');  
-        })
+        });
         
         opacity.addEventListener('click', function() {
             opacity.classList.add('hidden');
             btnMenu.classList.remove('hidden');   
             menu.classList.add('hidden'); 
             logo.classList.remove('logo-expanded');        
-        })
+        });
+
+        window.addEventListener('resize', function() {
+            if(window.innerWidth >= 800) {
+                menu.classList.remove('hidden')
+            } else if(window.innerWidth <= 800) {
+                menu.classList.add('hidden') 
+            }
+        });
+
+        window.addEventListener('load', function() {
+            if(window.innerWidth >= 800) {
+                menu.classList.remove('hidden')
+            } else if(window.innerWidth <= 800) {
+                menu.classList.add('hidden') 
+            }
+        });
+
+        window.addEventListener('scroll', function() {
+            if(document.body.scrollTop >= 150) {
+                menuContainer.classList.add('menu-fixed');
+            } else if (document.body.scrollTop <= 150) {
+                menuContainer.classList.remove('menu-fixed');
+            }
+        });
     }
 
     return init
